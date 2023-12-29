9a6077b1d0d9be21511e6eacef124009c732fac3
Thong Le <thong.le@cyberlogitec.com>
2023-12-29 16:01:23 +0700

    add resolver

--- projects/apollo/src/school/teacher.entity.ts
+++ projects/apollo/src/school/teacher.entity.ts
@@ -1,14 +1,19 @@
 import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Subject } from './subject.entity';
+import { ObjectType, Field } from '@nestjs/graphql';
 
 @Entity()
+@ObjectType()
 export class Teacher {
   @PrimaryGeneratedColumn()
+  @Field({ nullable: true })
   id: number;
 
   @Column()
+  @Field()
   name: string;
 
   @ManyToMany(() => Subject, (subject) => subject.teachers)
+  @Field(() => [Subject])
   subjects: Subject[];
 }
